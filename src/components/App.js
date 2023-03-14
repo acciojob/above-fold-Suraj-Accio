7fea250c70207ebc5f1ab0b67decb04864089181
Suraj-Accio <suraj@acciojob.com>
2023-03-14 20:21:54 +0530

    submitting code-Tue Mar 14 2023 20:21:54 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import './../styles/App.css';
+import 'regenerator-runtime/runtime'
 
 function App() {
   const [items, setItems] = useState([]);
